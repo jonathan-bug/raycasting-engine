0701a6a039aab357022814870d1bbb8ac6fc1c61
jonathan-bug <jonathanlima.dev@gmail.com>
2026-05-29 10:42:09 -0600

    feat: update engine

--- src/utils/Vector.ts
+++ src/utils/Vector.ts
@@ -33,20 +33,40 @@ export default class Vector {
         return this.x * vector.x + this.y * vector.y
     }
     
-    public scale(magnitude: number) {
+    public scale(magnitude: number): Vector {
         this.x *= magnitude
         this.y *= magnitude
 
         return this
     }
 
-    public unit() {
-        var magnitude = this.magnitude()
+    public unit(): Vector {
+        const magnitude = this.magnitude()
 
         if(magnitude == 0) {
-            return new Vector(0, 0)
+            this.x = 0
+            this.y = 0
+
+            return this
         }
 
-        return new Vector(this.x / magnitude, this.y / magnitude)
+        this.x /= magnitude
+        this.y /= magnitude
+
+        return this
+    }
+
+    public mutate(x: number, y: number) {
+        this.x = x
+        this.y = y
+
+        return this
+    }
+
+    public translate(x: number, y: number) {
+        this.x += x
+        this.y += y
+
+        return this
     }
 }
